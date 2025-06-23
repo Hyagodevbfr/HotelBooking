47d66de4e49c67ab069c23ffcc56ca15e4b1cf79
hyagodevbfr <hyago.sbrito@gmail.com>
2025-06-23 13:14:55 -0300

    feat: add mongo db extensions to API

---
+++ HotelBooking.API/Extensions/MongoDbExtensions.cs
@@ -0,0 +1,16 @@
+﻿using HotelBooking.Infrastructure.Data.Mongo;
+using HotelBooking.SharedKernel.Settings;
+
+namespace HotelBooking.API.Extensions
+{
+    public static class MongoDbExtensions
+    {
+        public static IServiceCollection AddMongoDb(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<MongoDbSettings>(configuration.GetSection("MongoDbSettings"));
+            services.AddSingleton<MongoDbContext>();
+
+            return services;
+        }
+    }
+}
