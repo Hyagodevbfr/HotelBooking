fe94731d23ea4f01f98f9f959d6582b91dc4df6b
hyagodevbfr <hyago.sbrito@gmail.com>
2025-06-23 13:15:48 -0300

    feat: create base entity and update hotelbooking solution

---
+++ HotelBooking.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using NanoidDotNet;
+
+namespace HotelBooking.Domain.Entities
+{
+    public class BaseEntity
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; private set; }
+
+        [BsonElement("PublicId")]
+        public string PublicId { get; private set; }
+
+        protected BaseEntity()
+        {
+            Id = ObjectId.GenerateNewId().ToString();
+            PublicId = Nanoid.Generate(size: 21);
+        }
+    }
+}
