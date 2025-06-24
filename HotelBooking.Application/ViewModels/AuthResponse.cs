212d8738628f4e0ec77605dc183217be29c5fc1a
hyagodevbfr <hyago.sbrito@gmail.com>
2025-06-24 15:41:15 -0300

    feat: create auth response view

---
+++ HotelBooking.Application/ViewModels/AuthResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace HotelBooking.Application.ViewModels
+{
+    public class AuthResponse
+    {
+        public string Token { get; private set; }
+        public bool IsSuccess { get; private set; }
+        public string Message { get; private set; }
+        public string RefreshToken { get; private set; }
+
+        public AuthResponse(string token, bool isSuccess, string message, string refreshToken)
+        {
+            Token = token;
+            IsSuccess = isSuccess;
+            Message = message;
+            RefreshToken = refreshToken;
+        }
+    }
+}
