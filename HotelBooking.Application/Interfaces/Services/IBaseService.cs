b6f44bac449cb3087ab1616857841a21e2cc315a
hyagodevbfr <hyago.sbrito@gmail.com>
2025-06-24 15:38:55 -0300

    feat: create base service interface

---
+++ HotelBooking.Application/Interfaces/Services/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelBooking.Application.Interfaces.Services
+{
+    public interface IBaseService<TResponseDto, TRegisterDto, TUpdateDto>
+    {
+        Task<TResponseDto> GetByPublicId(string id);
+        Task<IEnumerable<TResponseDto>> GetAll();
+        Task<TResponseDto> Create(TRegisterDto registerDto);
+        Task<TResponseDto> Update(TUpdateDto updateDto);
+        Task Delete(string id);
+
+    }
+}
