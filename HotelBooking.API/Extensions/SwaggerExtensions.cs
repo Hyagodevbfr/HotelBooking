411ec86b07ca76961509fe7eba11e3005cf85daf
hyagodevbfr <hyago.sbrito@gmail.com>
2025-06-24 15:46:00 -0300

    feat: create swagger extensions

---
+++ HotelBooking.API/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace HotelBooking.API.Extensions
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelBooking API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = "Please enter a valid token",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+            return services;
+        }
+    }
+}
