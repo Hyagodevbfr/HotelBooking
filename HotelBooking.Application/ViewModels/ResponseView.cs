0be74a82bd5ff1a527b29e86a668b9f037a953b5
hyagodevbfr <hyago.sbrito@gmail.com>
2025-06-24 15:41:30 -0300

    feat: create response view

---
+++ HotelBooking.Application/ViewModels/ResponseView.cs
@@ -0,0 +1,30 @@
+﻿namespace HotelBooking.Application.ViewModels
+{
+    public class ResponseView
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public object? Data { get; set; }
+
+        public ResponseView(bool isSuccess, string message, object? data = null)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+            Data = data;
+        }
+    }
+
+    public class ResponseView<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public T? Data { get; set; }
+
+        public ResponseView(bool isSuccess, string message, T? data = default)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+            Data = data;
+        }
+    }
+}
