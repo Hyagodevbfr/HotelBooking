a33d444b82296de5ba582cb8abc5cdc493ea21c9
hyagodevbfr <hyago.sbrito@gmail.com>
2025-06-24 15:45:39 -0300

    feat: create repository extensions

---
+++ HotelBooking.API/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using HotelBooking.Domain.Interfaces.Repositories;
+using HotelBooking.Infrastructure.Repositories;
+
+namespace HotelBooking.API.Extensions
+{
+    public static class RepositoryExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+    }
+}
