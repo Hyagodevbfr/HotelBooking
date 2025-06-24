9ecad3a0ba7878254bad7495fd77dad0311978a4
hyagodevbfr <hyago.sbrito@gmail.com>
2025-06-24 15:46:35 -0300

    feat: create base controller

---
+++ HotelBooking.API/Controllers/BaseController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBooking.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        public string Email
+        {
+            get
+            {
+                return this.User.Claims.FirstOrDefault(c => c.Type == "Email")!.Value;
+            }
+        }
+
+        public string Id
+        {
+            get
+            {
+                return this.User.Claims.FirstOrDefault(c => c.Type == "Id")!.Value;
+            }
+        }
+
+        public string UserLevel
+        {
+            get
+            {
+                return this.User.Claims.FirstOrDefault(c => c.Type == "UserLevel")!.Value;
+            }
+        }
+    }
+}
