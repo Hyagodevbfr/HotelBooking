b1a311a7decaae7108e2e6ab137282a9531eca06
hyagodevbfr <hyago.sbrito@gmail.com>
2025-06-24 15:37:38 -0300

    feat: create token configuration

---
+++ HotelBooking.Application/Configurations/TokenConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBooking.Application.Configurations
+{
+    public class TokenConfiguration
+    {
+        public string Audience { get; set; }
+        public string Issuer { get; set; }
+        public string Secret { get; set; }
+        public int TimeToExpiry { get; set; }
+    }
+}
