ab350f39c2549576b35d9e42afff08c55384f74a
hyagodevbfr <hyago.sbrito@gmail.com>
2025-06-24 15:40:45 -0300

    feat: implement user password service

---
+++ HotelBooking.Application/Services/UserPasswordHasherService.cs
@@ -0,0 +1,21 @@
+﻿using HotelBooking.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Identity;
+
+namespace HotelBooking.Application.Services
+{
+    public class UserPasswordHasherService : IUserPasswordHasher
+    {
+        private readonly PasswordHasher<object> _hasher = new();
+
+        public string HashPassword(string rawPassword)
+        {
+            return _hasher.HashPassword(null!, rawPassword);
+        }
+
+        public bool VerifyPassword(string hashedPassword, string rawPassword)
+        {
+            var result = _hasher.VerifyHashedPassword(null!, hashedPassword, rawPassword);
+            return result == PasswordVerificationResult.Success;
+        }
+    }
+}
