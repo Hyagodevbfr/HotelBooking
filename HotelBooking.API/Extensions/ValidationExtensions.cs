755a5d3948294d335a74c0c1158efeb3b56d9dc5
hyagodevbfr <hyago.sbrito@gmail.com>
2025-06-24 15:46:24 -0300

    feat: create validation extensions

---
+++ HotelBooking.API/Extensions/ValidationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using HotelBooking.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBooking.API.Extensions
+{
+    public static class ValidationExtensions
+    {
+        public static IServiceCollection AddValidation(this IServiceCollection services)
+        {
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    var result = new ResponseView<object>(
+                        isSuccess: false,
+                        message: "Invalid data provided.",
+                        data: null
+                        );
+                    return new BadRequestObjectResult(result);
+                };
+            });
+            return services;
+        }
+    }
+}
