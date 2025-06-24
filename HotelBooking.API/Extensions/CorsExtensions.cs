9f250a247963a4dc43faa6b7afa2dcb856f858aa
hyagodevbfr <hyago.sbrito@gmail.com>
2025-06-24 15:45:20 -0300

    feat: create cors extensions

---
+++ HotelBooking.API/Extensions/CorsExtensions.cs
@@ -0,0 +1,28 @@
+﻿using HotelBooking.Application.Configurations;
+
+namespace HotelBooking.API.Extensions
+{
+    public static class CorsExtensions
+    {
+        public static IServiceCollection AddCustomCors(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<CorsSettings>(configuration.GetSection("Cors"));
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    var corsOrigins = configuration.GetSection("Cors:Origins").Get<string[]>() ?? Array.Empty<string>();
+
+                    builder
+                        .WithOrigins(corsOrigins)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+            });
+
+            return services;
+        }
+    }
+}
