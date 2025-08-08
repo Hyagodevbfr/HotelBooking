eaee0bc1d5fda74e8b3c15dc8c55858289cd9517
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-08 17:49:25 -0300

    feat: include Quantity in Room mapping methods

--- HotelBooking.Application/Mappers/RoomMappingExtensions.cs
+++ HotelBooking.Application/Mappers/RoomMappingExtensions.cs
@@ -10,7 +10,9 @@ public static class RoomMappingExtensions
         return new RoomResponseDto(
             PublicId:  room.PublicId,
             Name: room.RoomName,
+            Description: room.Description,
             Capacity: room.Capacity,
+            Quantity: room.Quantity,
             PricePerNight: room.PricePerNight,
             IsAvailable: room.IsAvailable
         );
@@ -22,18 +24,20 @@ public static class RoomMappingExtensions
             response.Name,
             response.Description,
             response.Capacity,
+            response.Quantity,
             response.PricePerNight,
             response.IsAvailable
         );
     }
 
-    public static RoomUpdateDto ToUpdate(this Room room)
+    public static RoomUpdateDto ToEntity(this Room room)
     {
         return new RoomUpdateDto(
             room.PublicId.ToString(),
             room.RoomName,
             room.Description,
             room.Capacity,
+            room.Quantity,
             room.PricePerNight,
             room.IsAvailable
         );
